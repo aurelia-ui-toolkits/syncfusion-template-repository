41e12e70c1bbb0b90b44589cd7e33fa97638d39b
karthickthangasamy <karthickt@syncfusion.com>
2016-06-17 14:59:46 +0530

    feat(all):add syncfusion-es2016 skeleton navigation app

--- src/welcome.js
+++ src/welcome.js
@@ -1,7 +1,7 @@
 //import {computedFrom} from 'aurelia-framework';
 
 export class Welcome {
-  heading = 'Welcome to the Aurelia KendoUI-core Navigation App!';
+  heading = 'Welcome to the Aurelia Syncfusion Navigation App!';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
